1c8cca5f95179ccb13e29d88cd998a02d4299182
Krzycholl18 <32575999+krzychsol@users.noreply.github.com>
2023-03-17 17:56:12 +0100

    lab2 - done

---
+++ lab2/zadanie1.js
@@ -0,0 +1,14 @@
+/*
+window.prompt("Tekst1", "Tekst2");
+
+var wczytanaWartosc;
+for(let i=0;i<4;i++){
+    wczytanaWartosc = window.prompt("Podaj wartość:");
+    console.log(wczytanaWartosc+":"+typeof(wczytanaWartosc));
+}
+*/
+var selectedForm = document.forms['form_name'];
+function form_handler(){
+    console.log(selectedForm.pole_tekstowe.value+":"+typeof(selectedForm.pole_tekstowe.value));
+    console.log(selectedForm.pole_liczbowe.value+":"+typeof(selectedForm.pole_liczbowe.value));
+}
